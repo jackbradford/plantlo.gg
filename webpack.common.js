8d1dde576f3b3626f8b1f7ca9af901d299844630
Jack Brown <jack@jackbrown.io>
2020-02-06 11:56:20 -0800

    Added Webpack config, npm config

---
+++ webpack.common.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/plantlogg.js',
+  },
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Production',
+    }),
+  ],
+  output: {
+    filename: './htdocs/js/plantlogg.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
+
