76858dee6f2e4c35878bd71c3b7f25eb9f5481bf
Jack Brown <jack@jackbrown.io>
2020-02-06 14:40:05 -0800

    Changed where webpack outputs bundles.
    The files, which are all .js files, will go in htdocs/js.

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -9,13 +9,13 @@ module.exports = {
   plugins: [
     // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Production',
-    }),
+//    new HtmlWebpackPlugin({
+//      title: 'Production',
+//    }),
   ],
   output: {
-    filename: './htdocs/js/plantlogg.bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: './plantlogg.bundle.js',
+    path: path.resolve(__dirname, 'htdocs/js'),
   },
 };
 
