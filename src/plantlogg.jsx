14841a8574c9f8a5a6f50e3f72c6dadc7791d5cf
Jack Brown <jack@jackbrown.io>
2020-02-10 11:23:52 -0800

    Added src/plantlogg.jsx
    This is the main source for the front-end app. Webpack et. al. compiles
    this and bundles it into htdocs/js/plantlogg.bundle.js.

---
+++ src/plantlogg.jsx
@@ -0,0 +1,81 @@
+import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Route, Switch } from 'react-router-dom';
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { createBrowserHistory } from 'history';
+import rootReducer from './reducers';
+import Home from './component/pages/home';
+
+console.log('Running plantlogg.js');
+if (process.env.NODE_ENV !== 'production') {
+
+    console.log('Plantlogg.js running in DEVELOPMENT mode.');
+}
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        thunkMiddleware
+    )
+);
+const history = syncHistoryWithStore(createBrowserHistory(), store);
+
+/**
+ * App
+ * This is the main React component for the front-end app.
+ *
+ */
+class App extends Component {
+
+    render() {
+
+        return (
+            <Router history={history}>
+                <Root />
+            </Router>
+        );
+    }
+}
+
+/**
+ * Root
+ * This is the root of the app. It contains everything except the front-end
+ * router. It does, however, define the routes.
+ *
+ */
+class Root extends Component {
+
+    render() {
+
+        return (
+            <React.Fragment>
+                <Route exact path='/' component={Home} />
+                <Route path='/test' component={Test} />
+            </React.Fragment>
+        );
+    }
+}
+
+/**
+ * Test
+ * This was written to test the router.
+ *
+ */
+class Test extends Component {
+
+    render() {
+
+        return (
+            <div>
+                <h2>Test</h2>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render( <App />, document.getElementById('plantlogg') );
+
