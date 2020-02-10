28f5f4ebbf7ee198a04788a9834d55e49e846699
Jack Brown <jack@jackbrown.io>
2020-02-10 11:15:55 -0800

    Updated npm, webpack dependencies, configs
    `npm run build` will build a development front-end.
    `npm run build-prod` will build one for production.
    
    JSX can now be handled, Redux is in use.

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/plantlogg.js',
+    app: './src/plantlogg.jsx',
   },
   plugins: [
     // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
@@ -17,5 +17,19 @@ module.exports = {
     filename: './plantlogg.bundle.js',
     path: path.resolve(__dirname, 'htdocs/js'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
 };
 
