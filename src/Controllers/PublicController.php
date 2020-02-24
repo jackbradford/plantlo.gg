4970b1096915d8ec9a3cb28c03d64e9f11b38b7d
Jack Brown <jack@jackbrown.io>
2020-02-24 11:50:39 -0800

    Added activation emails.
    The system should now be capable of sending activation emails. The email
    server/login information can be configured via the disphatch.conf.json
    file under the "email" directive.
    
    This feature remains untested, however, since neither the production
    server nor the Email service have been setup.

--- src/Controllers/PublicController.php
+++ src/Controllers/PublicController.php
@@ -4,6 +4,7 @@ namespace JackBradford\Plantlogg;
 use JackBradford\Disphatch\Controllers\Controller;
 use JackBradford\Disphatch\Controllers\IRequestController;
 use JackBradford\Disphatch\Controllers\ControllerResponse;
+use JackBradford\Disphatch\Etc\Activation;
 
 class PublicController extends Controller implements IRequestController {
 
@@ -14,29 +15,34 @@ class PublicController extends Controller implements IRequestController {
 
     public function registerUser() {
 
-        require_once 'magic_unicorn';
-        $email = $this->fromPOST('emailAddress');
-        $username = $this->fromPOST('username');
-        $password = $this->fromPOST('password');
-        $firstName = $this->fromPOST('firstName');
-        $lastName = $this->fromPOST('lastName');
+        $data = json_decode($this->fromPOST('data'));
+        $email = $data->emailAddress;
+        $username = $data->username;
+        $firstName = $data->firstName;
+        $lastName = $data->lastName;
+
+        // TODO ensure username <36 characters.
 
         try {
 
             // Creates a Sentinel user but returns a Disphatch User.
-            $user = $this->userMgr->createUser(
-                $firstName,
-                $lastName,
-                $email,
-                $password
-            );
-
-            $success = true;
+            $user = $this->addUserRecord($data);
             $activation = $user->getActivation();
             $code = $activation->getDetails()->code;
-            $activation->sendActivationEmail($email);
+            $recipName = (!empty($firstName) && !empty($lastName))
+                ? $firstName . ' ' . $lastName
+                : $username;
+
+            $this->sendActivationEmail($activation, $email, $recipName);
+
+            /*
+            $activation->sendActivationEmail($email, $subject, $body, (object)[]);
+             */
+
+            $success = true;
             $data = (object)[
                 'user' => $user->getDetails(),
+                'username' => $username,
                 'activation_code' => $code,
             ];
             $cliMsg = 'User added successfully. Activation code: '
@@ -60,12 +66,55 @@ class PublicController extends Controller implements IRequestController {
 
         $data = json_decode($this->fromPOST('data'));
         $validator = $validators[$data->fieldType];
-        error_log($data->fieldType . " ||| " . $data->userInput);
         $result = $this->{ $validator }($data->userInput);
         return new ControllerResponse(
             $result->success,
             $result->message,
             $result
+        );
+    }
+
+    private function addUserRecord($userInfo) {
+
+        $user = $this->userMgr->createUser(
+            $userInfo->firstName,
+            $userInfo->lastName,
+            $userInfo->emailAddress,
+            $userInfo->password
+        );
+        if (!$this->db->getConnection('default')->insert(
+            'INSERT INTO usernames (username, user_id) VALUES (?, ?)',
+            [$userInfo->username, $user->getDetails()->id]
+        )) {
+            $this->userMgr->deleteUser($user->getDetails()->id);
+            throw new \Exception('Could not add username.');
+        }
+        return $user;
+    }
+
+    private function sendActivationEmail(Activation $activation, $email, $name) {
+
+        $userId = $activation->getDetails()->userId;
+        $code = $activation->getDetails()->code;
+        $link = "https://plantlo.gg/activate/" . $userId . '/' . $code;
+
+        $subject = 'Activate your PlantLogg account!';
+        $body = '<p>Thank you for joining PlantLogg!</p>';
+        $body .= "<p><a href=\"$link\">Click here to activate your account.</a></p>";
+
+        $emailConf = $this->config->getDirective('email');
+        $recipient = (object) ['address' => $email, 'name' => $name];
+        $activation->sendActivationEmail(
+            $subject,
+            $body,
+            (object)[
+                'host' => $emailConf->host,
+                'username' => $emailConf->username,
+                'password' => $emailConf->password,
+                'fromAddress' => $emailConf->fromAddress,
+                'fromName' => $emailConf->fromName,
+                'recipients' => [$recipient]
+            ]
         );
     }
 
